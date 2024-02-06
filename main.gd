48aad2faf807f03e77a6835b4ba5272effc7fe34
Elrick <alexis.leviavant@gmail.com>
2024-02-06 17:53:27 +0100

    Initial commit

---
+++ main.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var world: Array = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+func test_adjacency(y, x):
+	return 2
+
+func generate():
+	var temp_world = world
+	for y in world:
+		for x in world:
+			var adjacent = test_adjacency(y, x)
+	world = temp_world
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		generate()
